06f185c9088534bf6ffde5c32d2014eb98915fcd
andy2great <andy2greatu2@gmail.com>
2021-02-27 22:24:11 -0500

    using express

--- app.js
+++ app.js
@@ -1,10 +1,39 @@
-const http = require("http");
+const express = require("express");
+const bodyParser = require("body-parser");
+const cors = require("cors");
+
+const app = express(); // app gère les endpoints
+const jsonParser = bodyParser.json(); // parse JSON
+const port = 54545; // Important: Défini le port ici
 const nodeCmd = require("node-cmd");
 
-const hostname = "localhost";
-const port = 54545;
+app.use(
+  cors({
+    origin: function (origin, callback) {
+      /*if (!origin) return callback(null, true)
+              if (allowedOrigins.indexOf(origin) === -1) {
+                  var msg =
+                      "The CORS policy for this site does not " +
+                      "allow access from the specified Origin."
+                  return callback(new Error(msg), false)
+              }*/
+      return callback(null, true);
+    },
+  })
+);
 
-const server = http.createServer((req, res) => {
+// tout au long, utilise JSON parser
+app.use(jsonParser);
+
+app.listen(port, () => {
+  console.log(`Listening at http://localhost:${port}`);
+});
+
+app.post("*", (req, res) => {
+  console.log(req);
+});
+
+/*const server = http.createServer((req, res) => {
   cmdCall("cd ..\\GeneratorSensorGateringInterface\\ && ls && sudo git pull")
     .then(() =>
       cmdCall("cd ..\\GeneratorSensorGateringServer\\ && ls && sudo git pull")
@@ -13,11 +42,7 @@ const server = http.createServer((req, res) => {
     .then(() => cmdCall("sudo docker-compose down"))
     .then(() => cmdCall("sudo docker-compose build"))
     .then(() => cmdCall("sudo docker-compose up"));
-});
-
-server.listen(port, hostname, () => {
-  console.log(`Server running at http://${hostname}:${port}/`);
-});
+});*/
 
 const cmdCall = (req) => {
   return new Promise((resolve) => {
